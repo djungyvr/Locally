abdade9998df098175bde217529fdd0cd1ed4de7
alin9162 <andylin1996@gmail.com>
2016-11-30 20:40:35 -0800

    Change About Us page to Intent and add app slogan

---
+++ app/src/main/java/com/example/djung/locally/View/AboutUsActivity.java
@@ -0,0 +1,15 @@
+package com.example.djung.locally.View;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.djung.locally.R;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_about_us_activity);
+    }
+}
