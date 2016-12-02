8501a99a857d9a48ee228c802a3a348c9325a3b1
u5n8 <poivre@mail.ru>
2016-12-01 17:29:39 -0800

    Polished calendar design

--- app/src/main/java/com/example/djung/locally/View/Adapters/MarketPageAdapter.java
+++ app/src/main/java/com/example/djung/locally/View/Adapters/MarketPageAdapter.java
@@ -44,7 +44,7 @@ public class MarketPageAdapter extends RecyclerView.Adapter<MarketPageAdapter.Vi
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_market, parent, false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.market_fragment, parent, false);
         return new ViewHolder(v, context, markets);
     }
 
