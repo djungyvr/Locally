3d14f9cc3391801892c54fb099e7ed3d41beb7a8
David Jung <d.jung460@gmail.com>
2016-12-02 08:35:06 -0800

    Put back local caching for images

--- app/src/main/java/com/example/djung/locally/View/EditVendorDetailsFragment.java
+++ app/src/main/java/com/example/djung/locally/View/EditVendorDetailsFragment.java
@@ -178,9 +178,7 @@ public class EditVendorDetailsFragment extends Fragment implements View.OnClickL
         // Load the image
         Picasso.with(getContext()).setIndicatorsEnabled(true);
         Picasso.with(getContext()).setLoggingEnabled(true);
-        Picasso.with(getContext()).setLoggingEnabled(true);
         Picasso.with(getContext()).load(vendorPhotoUrl)
-                .memoryPolicy(MemoryPolicy.NO_CACHE)
                 .networkPolicy(NetworkPolicy.NO_CACHE)
                 .error(R.drawable.ic_broken_image)
                 .into(mImageViewVendor);
