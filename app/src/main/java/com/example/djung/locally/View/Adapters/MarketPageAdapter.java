82b45ac46d03e21252d8c6ee6b86a996215d95a2
David Jung <d.jung460@gmail.com>
2016-12-04 12:10:41 -0800

    Replace market_fragment base layout to a coordinator layout

--- app/src/main/java/com/example/djung/locally/View/Adapters/MarketPageAdapter.java
+++ app/src/main/java/com/example/djung/locally/View/Adapters/MarketPageAdapter.java
@@ -4,15 +4,16 @@ import android.content.Context;
 import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.text.util.Linkify;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView;
 import android.widget.TextView;
 
 import com.example.djung.locally.Model.Market;
 import com.example.djung.locally.R;
 import com.example.djung.locally.Utils.MarketUtils;
+
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -32,30 +33,33 @@ import static com.example.djung.locally.Utils.DateUtils.parseYear;
  */
 
 public class MarketPageAdapter extends RecyclerView.Adapter<MarketPageAdapter.ViewHolder> implements OnMapReadyCallback {
-    private ArrayList<Market> markets;
-    private Context context;
-    private GoogleMap googleMap;
-    private LatLng latLng;
+    private final String TAG = "MarketPageAdapter";
+    private ArrayList<Market> mMarkets;
+    private Context mContext;
+    private GoogleMap mGoogleMap;
+    private LatLng mLatLng;
 
     public MarketPageAdapter(ArrayList<Market> markets, Context context) {
-        this.markets = markets;
-        this.context = context;
+        mMarkets = markets;
+        mContext = context;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.market_fragment, parent, false);
-        return new ViewHolder(v, context, markets);
+        return new ViewHolder(v, mContext, mMarkets);
     }
 
     //Fill in page elements with data to display
     @Override
     public void onBindViewHolder(MarketPageAdapter.ViewHolder holder, int position) {
-        Market marketItem = markets.get(position);
-        holder.marketName.setText(marketItem.getName());
-        holder.marketLocation.setText("Location: " + marketItem.getAddress());
-        holder.marketWeekDays.setText(parseHours(marketItem.getDailyHours()));
-        holder.marketYearOpen.setText(parseYear(marketItem.getYearOpen()));
+        Market marketItem = mMarkets.get(position);
+        holder.mMarketName.setText(marketItem.getName());
+        holder.mMarketDescriptions.setText(marketItem.getDescription());
+        Log.d(TAG,marketItem.getDescription());
+        holder.mMarketLocation.setText("Location: " + marketItem.getAddress());
+        holder.mMarketWeekDays.setText(parseHours(marketItem.getDailyHours()));
+        holder.mMarketYearOpen.setText(parseYear(marketItem.getYearOpen()));
 
         if (holder.mapPreview != null) {
             holder.mapPreview.onCreate(null);
@@ -63,70 +67,65 @@ public class MarketPageAdapter extends RecyclerView.Adapter<MarketPageAdapter.Vi
             holder.mapPreview.getMapAsync(this);
         }
 
-        latLng = new LatLng(marketItem.getLatitude(), marketItem.getLongitude());
-
-        //Justify the market description text
-        String htmlText = "<html><body style=\"text-align:justify\"> %s </body></Html>";
-        String descriptionText = marketItem.getDescription();
-        holder.marketDescription.loadData(String.format(htmlText, descriptionText), "text/html; charset=UTF-8", null);
+        mLatLng = new LatLng(marketItem.getLatitude(), marketItem.getLongitude());
 
         //set the link for directions to open in Google maps
         String strUri = "http://maps.google.com/maps?q=loc:" + marketItem.getLatitude() + "," + marketItem.getLongitude();
         Pattern pattern = Pattern.compile("[a-zA-Z]+");
-        Linkify.addLinks(holder.marketDirections, pattern, strUri);
+        Linkify.addLinks(holder.mMarketDirections, pattern, strUri);
 
         if (MarketUtils.isMarketCurrentlyOpen(marketItem)) {
-            holder.marketStatus.setText("Open Now!");
+            holder.mMarketStatus.setText("Open Now!");
         } else {
-            holder.marketStatus.setText("Closed Now");
+            holder.mMarketStatus.setText("Closed Now");
         }
     }
 
     @Override
     public void onMapReady(GoogleMap map) {
-        googleMap = map;
+        mGoogleMap = map;
         // Add a marker for market
-        googleMap.addMarker(new MarkerOptions().position(latLng));
-        CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(latLng, 13);
-        googleMap.animateCamera(yourLocation);
+        mGoogleMap.addMarker(new MarkerOptions().position(mLatLng));
+        CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(mLatLng, 13);
+        mGoogleMap.animateCamera(yourLocation);
     }
 
 
     @Override
     public int getItemCount() {
-        if (markets != null) {
-            return markets.size();
+        if (mMarkets != null) {
+            return mMarkets.size();
         } else {
             return 0;
         }
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        public TextView marketName;
-        public TextView marketStatus;
-        public TextView marketLocation;
-        public TextView marketWeekDays;
-        public TextView marketYearOpen;
-        public TextView marketDirections;
-        public WebView marketDescription;
+        public TextView mMarketName;
+        public TextView mMarketStatus;
+        public TextView mMarketLocation;
+        public TextView mMarketWeekDays;
+        public TextView mMarketYearOpen;
+        public TextView mMarketDirections;
+        public TextView mMarketDescriptions;
         public MapView mapPreview;
-        public Context context;
-        public ArrayList<Market> markets;
+        public Context mContext;
+        public ArrayList<Market> mMarkets;
 
         public ViewHolder(View v, Context context, ArrayList<Market> markets) {
             super(v);
-            this.context = context;
-            this.markets = markets;
-            this.marketName = (TextView) v.findViewById(R.id.market_name);
-            this.marketStatus = (TextView) v.findViewById(R.id.market_status);
-            this.marketLocation = (TextView) v.findViewById(R.id.market_location);
-            this.marketWeekDays = (TextView) v.findViewById(R.id.market_hours);
-            this.marketYearOpen = (TextView) v.findViewById(R.id.market_year_open);
-            this.mapPreview = (MapView) v.findViewById(R.id.market_map_preview);
-            this.marketDescription = (WebView) v.findViewById(R.id.market_description);
+            mContext = context;
+            mMarkets = markets;
+            mMarketName = (TextView) v.findViewById(R.id.market_name);
+            mMarketStatus = (TextView) v.findViewById(R.id.market_status);
+            mMarketLocation = (TextView) v.findViewById(R.id.market_location);
+            mMarketWeekDays = (TextView) v.findViewById(R.id.market_hours);
+            mMarketYearOpen = (TextView) v.findViewById(R.id.market_year_open);
+            mapPreview = (MapView) v.findViewById(R.id.market_map_preview);
+            mMarketDescriptions = (TextView) v.findViewById(R.id.market_description);
 
-            this.marketDirections = (TextView) v.findViewById(R.id.market_directions);
-            marketDirections.setLinkTextColor(Color.parseColor("#4C516D"));
+            mMarketDirections = (TextView) v.findViewById(R.id.market_directions);
+            mMarketDirections.setLinkTextColor(Color.parseColor("#4C516D"));
             itemView.setOnClickListener(this);
         }
 
